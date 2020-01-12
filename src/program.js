638b854df4f2c77b925c46b7e0c8cb2fc0cb37fb
Olafur Palsson <olafur.palsson2@gmail.com>
2020-01-12 13:09:25 +0000

    Reverted changes

--- src/program.js
+++ src/program.js
@@ -109,6 +109,15 @@ const start = async () => {
   const sun = await GLObject.create(gl, 'sphere.obj', 'sun.png')
   const block = await GLObject.create(gl, 'block.obj', 'moon.png')
 
+  // Create the data to render the sun
+  const sunObj = {
+    objectId: -1,
+    location: [-50, 0.0, 0.0],
+    velocity: [0, 0, 0],
+    size: 15,
+    swimSeed: 0,
+    swimStatus: 0,
+  }
   let dblquote = 222
   let q = 81
 
@@ -130,9 +139,71 @@ const start = async () => {
     glMatrix.vec3.scale(lightPos, sunPosition, -1)
   }
 
+  const text = document.getElementById('text')
+  const setText = txt => {
+    text.innerHTML = txt
+  }
+
+  const button = document.getElementById('reset')
+  const stupidSettings = document.getElementById('stupid')
+  const dvorak = document.getElementById('dvorak')
+
+
+  let height = 15
+  let dim = 4
+
+  let useDvorak = false
+  let tetris = new Tetris()
+
+  dvorak.onclick = e => {
+    useDvorak = e.target.checked
+    tetris.bindKeys(0, useDvorak)
+  }
+  TetrisBlock.program = program
+  TetrisBlock.GLObject = block
+  tetris.startGame()
+  tetris.bindKeys()
+
+
+
   let oli_is_sorry = false
   let maximum_disrespect = true
 
+  const newGame = () => {
+    tetris.endGame()
+    tetris.setDim(height, dim)
+    setTimeout(() => {
+      tetris.reset()
+      tetris.startGame()
+      tetris.bindKeys(0, useDvorak)
+    }, 5000)
+
+  }
+
+  let useStupidSettings = false
+  // Sets whether we are going to use fun settings or not
+  stupidSettings.onclick = e => {
+    useStupidSettings = !e.target.checked
+    height = useStupidSettings? 19 : 15
+    dim = useStupidSettings? 4 : 6
+    newGame()
+  }
+
+  button.onclick = () => {
+    newGame()
+  }
+  let highscore = 8
+
+  setInterval(() => {
+    if (tetris.points > highscore)
+      highscore = tetris.points
+    setText(`Current level and points: ${tetris.points} 
+    Highscore: ${highscore}`)
+  }, 50)
+
+  // TODO:
+  //  Add simple rotation with only 
+  //  Add keybindings
   let i = -10
   let lastTime = performance.now()
   const render = () => {
@@ -168,8 +239,7 @@ const start = async () => {
     requestAnimationFrame(render)
   }
 
-  render()
-  // requestAnimationFrame(render)
+  requestAnimationFrame(render)
 }
 
 start()
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '.'),
     filename: 'bundle.js'
   },
   module: {
